2d5c3a4b786d0c3176d73be63df6c85f64ded283
JESSICA JINNETH ESCAMILLA CARRERO <jescamilla02@unisalle.edu.co>
2022-10-05 23:02:51 -0500

    se termino la logica

--- app/inbox.py
+++ app/inbox.py
@@ -18,55 +18,58 @@ def getDB():
 def show():
     db = get_db()
     messages = db.execute(
-        'select * from message where to_id =?',(g.user['id'])
+        'select *, user.username from message inner join user on user.id = message.from_id where to_id = ?',(g.user['id'], )
     ).fetchall()
 
     return render_template('inbox/show.html', messages=messages)
 
 
-@bp.route('/send', methods=('GET', 'POST'))
+@bp.route('/send', methods=['GET', 'POST'])
 @login_required
 def send():
-    if request.method == 'POST':        
-        from_id = g.user['id']
-        to_username = request.form['to_username'] 
-        subject = request.form['subject'] 
-        body = request.form['body'] 
+    try:
+        if request.method == 'POST':        
+            from_id = g.user['id']
+            to_username = request.form['to'] 
+            subject = request.form['subject'] 
+            body = request.form['body'] 
 
-        db = get_db()
-       
-        if not to_username:
-            flash('To field is required')
-            return render_template('inbox/send.html')
-        
-        if not subject:
-            flash('Subject field is required')
-            return render_template('inbox/send.html')
-        
-        if not body:
-            flash('Body field is required')
-            return render_template('inbox/send.html')    
-        
-        error = None    
-        userto = None 
-        
-        userto = db.execute(
-            QUERY, (to_username,)
-        ).fetchone()
-        
-        if userto is None:
-            error = 'Recipient does not exist'
-     
-        if error is not None:
-            flash(error)
-        else:
             db = get_db()
-            db.execute(
-                QUERY,
-                (g.user['id'], userto['id'], subject, body)
-            )
-            db.commit()
+        
+            if not to_username:
+                flash('To field is required')
+                return render_template('inbox/send.html')
+            
+            if not subject:
+                flash('Subject field is required')
+                return render_template('inbox/send.html')
+            
+            if not body:
+                flash('Body field is required')
+                return render_template('inbox/send.html')    
+            
+            error = None    
+            userto = None 
+            
+            userto = db.execute(
+                'select * from user where username = ?', (to_username,)
+            ).fetchone()
+            
+            if userto is None:
+                error = 'Recipient does not exist'
+        
+            if error is not None:
+                flash(error)
+            else:
+                db = get_db()
+                db.execute(
+                    'insert into message (from_id, to_id, subject, body) values(?,?,?,?)',
+                    (from_id, userto['id'], subject, body)
+                )
+                db.commit()
 
-            return redirect(url_for('inbox.show'))
+                return redirect(url_for('inbox.show'))
+    except Exception as e:
+        flash(str(e))
 
     return render_template('inbox/send.html')
