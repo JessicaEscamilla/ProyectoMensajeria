2e1b34731bcd1b9c909836665b6b21cfd1e46fe2
JESSICA JINNETH ESCAMILLA CARRERO <jescamilla02@unisalle.edu.co>
2022-10-10 17:14:03 -0500

    update email credentials

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','hlarrotta@uninorte.edu.co', 'XXXXXX');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','jjescamilla@uninorte.edu.co', 'zxcvbnm1A.');
